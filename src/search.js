d1e1eaf15d7a8e56e52ed1feca4e3869ab0c2635
lehtaku <akulehtonen@hotmail.com>
2018-12-18 00:05:25 +0200

    Update volume adjustment

--- src/search.js
+++ src/search.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 let getYtVideos = (keyWord, callback) => {
     request({
-        url: `${config.googleBaseURL}part=${config.part}&maxResults=${config.maxResults}&q=${keyWord}&type=${config.type}&relevanceLanguage=${config.lang}&key=${process.env.API_KEY}`,
+        url: `${config.googleBaseURL}part=${config.part}&maxResults=${config.maxResults}&q=${keyWord}&type=${config.type}&key=${process.env.API_KEY}&relevanceLanguage=${config.lang}`,
         json: true
     }, (error, response, body) => {
         if (error) {
